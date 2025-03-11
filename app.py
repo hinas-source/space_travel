1a08f449cf4a8d093c535e8204bf8a0d30e633b3
Syeda Hina <135191919+hinas-source@users.noreply.github.com>
2025-03-11 08:50:39 +0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from supabase import create_client, Client
 import random
 
 # Initialize Supabase client
-SUPABASE_URL = st.secrets["supbase"]["SUPABASE_URL"]
+SUPABASE_URL = st.secrets["supabase"]["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["supabase"]["SUPABASE_KEY"]
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
